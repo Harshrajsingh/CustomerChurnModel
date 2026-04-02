c012c849e93b2215ddd93411bb00433a8e25faa3
Harshrajsingh <harshrajsingh1107@gmail.com>
2026-04-02 15:14:49 +0530

    Update CustomerChurnModel.py

--- CustomerChurnModel.py
+++ CustomerChurnModel.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 
+password="Harsh raj Singh"
 
 # In[2]:
 #print("Hello,This code is developed by Harsh")
